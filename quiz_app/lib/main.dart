8be957934d66585ef5049de4a6af67a851d6f822
Rishabh kumar <rsk1998200@gmail.com>
2020-12-05 17:51:30 +0530

    Basic App Created + BottomNavBar Created

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MaterialApp(
+    title: 'AskMe',
+    theme: ThemeData(primarySwatch: Colors.black),
+    // darkTheme: ThemeData(primaryColorDark: Colors.grey),
+    debugShowCheckedModeBanner: false,
+    home: MyHomePage(),
+  ));
 }
 
-class Myapp extends StatelessWidget {
-  var questionIndex = 0;
-  void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
-  }
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedItem = 0;
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['What is your Name ?', 'What is your favorite animal ?'];
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Quiz App'),
-        ),
-        body: Column(
-          children: [
-            Text(questions[questionIndex]),
-            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer2'), onPressed: answerQuestion)
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AskMe'),
+        centerTitle: true,
+        elevation: 20.0,
+        backgroundColor: Colors.teal,
+        shadowColor: Colors.grey,
+      ),
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_sharp),
+            label: 'Profile',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.),
+          //   label: 'Home',
+          // ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'About',
+          ),
+        ],
+        currentIndex: _selectedItem,
+        onTap: (setValue) {
+          setState(() {
+            _selectedItem = setValue;
+          });
+        },
       ),
     );
   }
